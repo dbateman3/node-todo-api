278f83d273af8a5f5e985acb3aca1de78605054b
Dan Bateman <dbateman0319@starkstate.net>
2017-12-30 22:43:58 -0500

    Testing GET todo/:ID

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'First test todo'
 }, {
+	_id: new ObjectID(),
 	text: 'Second test todo'
 }];
 
@@ -73,6 +76,31 @@ describe('Get /todos', function() {
 			expect(res.body.todos.length).toBe(2);
 
 		})
+		.end(done);
+	});
+});
+
+describe('Get /todos/:id', function() {
+	it('should return todo doc', function(done) {
+		request(app)
+		.get(`/todos/${todos[0]._id.toHexString()}`)
+		.expect(200)
+		.expect(function(res) {
+			expect(res.body.todo.text).toBe(todos[0].text);
+		})
+		.end(done);
+	});
+	it('should return 404 if todo not found', function(done) {
+		let newId = new ObjectID();
+		request(app)
+		.get(`/todos/${newId.toHexString()}`)
+		.expect(404)
+		.end(done);
+	});
+	it('should return 404 for non objectid', function(done) {
+		request(app)
+		.get(`/todos/123`)
+		.expect(404)
 		.end(done);
 	});
 });
